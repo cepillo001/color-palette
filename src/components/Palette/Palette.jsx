0d08fb214ad3aa4f773ad30bcb91ccb834e83925
Jose Luis Pintado <joseluispintado666@gmail.com>
2022-09-24 12:08:13 -0300

    uso de React Router

--- src/components/Palette/Palette.jsx
+++ src/components/Palette/Palette.jsx
@@ -1,21 +1,22 @@
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FavoritesContext } from '../../context/FavoriteContext';
 import './Palette.css';
 
 const Palette = ({ palette }) => {
-    const { id, name, colors, liked } = palette
-    const { favorites, setFavorites } = useContext(FavoritesContext);
-    const [isFavorite, setIsFavorite] = useState(liked);
-  
+  const { id, name, colors, liked } = palette
+  const { favorites, setFavorites } = useContext(FavoritesContext);
+  const [isFavorite, setIsFavorite] = useState(liked);
 
+  //TODO tienes que modificar el like en la paleta
   const handleFavorite = () => {
     setIsFavorite((isFavorite) => !isFavorite);
 
     //busco si la paleta ya esta en favoritos
     const foundIndex = favorites.findIndex(fav => fav.id === id);
 
-    //para agregar a favoritos (-1 significa que no existe)
+    //para agregar a favoritos
     if (foundIndex === -1) {
       setFavorites([...favorites, palette])
       return
@@ -28,9 +29,8 @@ const Palette = ({ palette }) => {
   }
 
 
-
   return (
-<div className='palette-container'>
+    <div className='palette-container'>
       <div className='palette'>
         <h3>{name}</h3>
         {colors.map((color) => {
@@ -45,16 +45,21 @@ const Palette = ({ palette }) => {
           );
         })}
       </div>
-      <div className='fav'>
-        {isFavorite ? (
-          <FaHeart className='fav heart' onClick={handleFavorite}/>
-        ) : (
-          <FaRegHeart className='fav' onClick={handleFavorite}/>
-        )}
+      <div className='palette-actions'>
+        <div className='fav'>
+          {isFavorite ? (
+            <FaHeart className='fav heart' onClick={handleFavorite}/>
+          ) : (
+            <FaRegHeart className='fav' onClick={handleFavorite}/>
+          )}
+        </div>
+        <Link className='btn-see-more' to={`/palette/${id}`}>
+            Ver más
+        </Link>
       </div>
-    </div>
 
-  );
+    </div>
+  )
 }
 
 export default Palette
