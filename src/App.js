6e5fae190078b94089ba53d2199df9f77dc7d9bc
Jose Luis Pintado <joseluispintado666@gmail.com>
2022-09-30 18:22:53 -0300

    cambios clase 07 - formulario

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Home from './routes/Home/Home';
 import PaletteDisplay from './routes/Palette/PaletteDisplay';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect} from 'react';
 import { getColorPalettes } from './service';
 import { ColorPalettesContext} from './context/ColorPalettesContext'
+import Navigation from './routes/Navigation/Navigation';
+import Login from './routes/Login/Login';
+import PaletteCreation from './routes/Palette/PaletteCreation';
 
 function App() {
-  const [colorPalettes, setColorPalettes] = useState([]);
+
+  const {setColorPalettes} = useContext(ColorPalettesContext)
+  
   useEffect(()=> {
     getColorPalettes()
     .then((data) => {
@@ -18,13 +23,14 @@ function App() {
 
   return (
       <div className='App'>
-        <ColorPalettesContext.Provider value={{colorPalettes, setColorPalettes}}>
-          <Routes>
-            {/* // aca implementa rutas, renderiza los componentes (Home) */}
-            <Route path='/' element={<Home/>}/> 
-            <Route path='/palette/:id' element={<PaletteDisplay/>}/>
-          </Routes>
-        </ColorPalettesContext.Provider>
+        <Routes>
+          <Route path='/' element={<Navigation/>}>
+            <Route index element={<Home/>}/>
+            <Route path='login' element={<Login/>}/>
+            <Route path='palette/:id' element={<PaletteDisplay/>}/>
+            <Route path='palette/create' element={<PaletteCreation/>}/>
+          </Route>
+        </Routes>
       </div>
   );
 }
